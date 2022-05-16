851adb3755ff1683970dfce6fdd59a845e185d94
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-16 15:27:04 +0200

    update func setupPipeline()

--- engine/source/runtime/function/render/source/vulkan_manager/passes/stecil_outline.cpp
+++ engine/source/runtime/function/render/source/vulkan_manager/passes/stecil_outline.cpp
@@ -3,8 +3,10 @@
 #include "runtime/function/render/include/render/vulkan_manager/vulkan_misc.h"
 #include "runtime/function/render/include/render/vulkan_manager/vulkan_passes.h"
 #include "runtime/function/render/include/render/vulkan_manager/vulkan_util.h"
-
+#include <stencil_solid_vert.h>
 #include <stencil_solid_frag.h>
+#include <stencil_scale_vert.h>
+#include <stencil_scale_frag.h>
 
 namespace Pilot {
 
@@ -48,7 +50,205 @@ namespace Pilot {
     }
 
     void PStencilOutlinePass::setupPipelines()
-    { }
+    {
+        _render_pipelines.resize(2);
+
+        VkDescriptorSetLayout      descriptorset_layouts[1] = { _descriptor_infos[0].layout };
+        VkPipelineLayoutCreateInfo pipeline_layout_create_info{};
+        pipeline_layout_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipeline_layout_create_info.setLayoutCount = 1;
+        pipeline_layout_create_info.pSetLayouts = descriptorset_layouts;
+
+        if (vkCreatePipelineLayout(
+            m_p_vulkan_context->_device, &pipeline_layout_create_info, nullptr, &_render_pipelines[0].layout) !=
+            VK_SUCCESS)
+        {
+            throw std::runtime_error("create stencil outline post process pipeline layout");
+        }
+
+        // common
+        VkPipelineVertexInputStateCreateInfo vertex_input_state_create_info{};
+        vertex_input_state_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertex_input_state_create_info.vertexBindingDescriptionCount = 0;
+        vertex_input_state_create_info.pVertexBindingDescriptions = NULL;
+        vertex_input_state_create_info.vertexAttributeDescriptionCount = 0;
+        vertex_input_state_create_info.pVertexAttributeDescriptions = NULL;
+
+        VkPipelineInputAssemblyStateCreateInfo input_assembly_create_info{};
+        input_assembly_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        input_assembly_create_info.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_STRIP;
+        input_assembly_create_info.primitiveRestartEnable = VK_FALSE;
+
+        VkPipelineViewportStateCreateInfo viewport_state_create_info{};
+        viewport_state_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewport_state_create_info.viewportCount = 1;
+        viewport_state_create_info.pViewports = &m_command_info._viewport;
+        viewport_state_create_info.scissorCount = 1;
+        viewport_state_create_info.pScissors = &m_command_info._scissor;
+
+        VkPipelineRasterizationStateCreateInfo rasterization_state_create_info{};
+        rasterization_state_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterization_state_create_info.depthClampEnable = VK_FALSE;
+        rasterization_state_create_info.rasterizerDiscardEnable = VK_FALSE;
+        rasterization_state_create_info.polygonMode = VK_POLYGON_MODE_FILL;
+        rasterization_state_create_info.lineWidth = 1.0f;
+        rasterization_state_create_info.cullMode = VK_CULL_MODE_BACK_BIT;
+        rasterization_state_create_info.frontFace = VK_FRONT_FACE_CLOCKWISE;
+        rasterization_state_create_info.depthBiasEnable = VK_FALSE;
+        rasterization_state_create_info.depthBiasConstantFactor = 0.0f;
+        rasterization_state_create_info.depthBiasClamp = 0.0f;
+        rasterization_state_create_info.depthBiasSlopeFactor = 0.0f;
+
+        VkPipelineMultisampleStateCreateInfo multisample_state_create_info{};
+        multisample_state_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisample_state_create_info.sampleShadingEnable = VK_FALSE;
+        multisample_state_create_info.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+        VkPipelineColorBlendAttachmentState color_blend_attachment_state{};
+        color_blend_attachment_state.colorWriteMask =
+            VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        color_blend_attachment_state.blendEnable = VK_FALSE;
+        color_blend_attachment_state.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
+        color_blend_attachment_state.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;
+        color_blend_attachment_state.colorBlendOp = VK_BLEND_OP_ADD;
+        color_blend_attachment_state.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+        color_blend_attachment_state.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+        color_blend_attachment_state.alphaBlendOp = VK_BLEND_OP_ADD;
+
+        VkPipelineColorBlendStateCreateInfo color_blend_state_create_info{};
+        color_blend_state_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        color_blend_state_create_info.logicOpEnable = VK_FALSE;
+        color_blend_state_create_info.logicOp = VK_LOGIC_OP_COPY;
+        color_blend_state_create_info.attachmentCount = 1;
+        color_blend_state_create_info.pAttachments = &color_blend_attachment_state;
+        color_blend_state_create_info.blendConstants[0] = 0.0f;
+        color_blend_state_create_info.blendConstants[1] = 0.0f;
+        color_blend_state_create_info.blendConstants[2] = 0.0f;
+        color_blend_state_create_info.blendConstants[3] = 0.0f;
+
+        VkDynamicState dynamic_states[] = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
+
+        VkPipelineDynamicStateCreateInfo dynamic_state_create_info{};
+        dynamic_state_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+        dynamic_state_create_info.dynamicStateCount = 2;
+        dynamic_state_create_info.pDynamicStates = dynamic_states;
+
+
+        // pass 1: solid color
+        VkShaderModule vert_shader_module =
+            PVulkanUtil::createShaderModule(m_p_vulkan_context->_device, STENCIL_SOLID_VERT);
+        VkShaderModule frag_shader_module =
+            PVulkanUtil::createShaderModule(m_p_vulkan_context->_device, STENCIL_SOLID_FRAG);
+        VkPipelineShaderStageCreateInfo vert_pipeline_shader_stage_create_info{};
+        vert_pipeline_shader_stage_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vert_pipeline_shader_stage_create_info.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vert_pipeline_shader_stage_create_info.module = vert_shader_module;
+        vert_pipeline_shader_stage_create_info.pName = "main";
+
+        VkPipelineShaderStageCreateInfo frag_pipeline_shader_stage_create_info{};
+        frag_pipeline_shader_stage_create_info.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        frag_pipeline_shader_stage_create_info.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        frag_pipeline_shader_stage_create_info.module = frag_shader_module;
+        frag_pipeline_shader_stage_create_info.pName = "main";
+
+        VkPipelineShaderStageCreateInfo shader_stages[] = { vert_pipeline_shader_stage_create_info,
+                                                           frag_pipeline_shader_stage_create_info };
+
+        VkPipelineDepthStencilStateCreateInfo depth_stencil_create_info{};
+        depth_stencil_create_info.stencilTestEnable = VK_TRUE;
+        depth_stencil_create_info.back.compareOp = VK_COMPARE_OP_ALWAYS;
+        depth_stencil_create_info.back.failOp = VK_STENCIL_OP_REPLACE;
+        depth_stencil_create_info.back.depthFailOp = VK_STENCIL_OP_REPLACE;
+        depth_stencil_create_info.back.passOp = VK_STENCIL_OP_REPLACE;
+        depth_stencil_create_info.back.compareMask = 0xff;
+        depth_stencil_create_info.back.writeMask = 0xff;
+        depth_stencil_create_info.back.reference = 1;
+        depth_stencil_create_info.front = depth_stencil_create_info.back;
+
+        VkGraphicsPipelineCreateInfo pipelineInfo{};
+        pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipelineInfo.stageCount = 2;
+        pipelineInfo.pStages = shader_stages;
+        pipelineInfo.pVertexInputState = &vertex_input_state_create_info;
+        pipelineInfo.pInputAssemblyState = &input_assembly_create_info;
+        pipelineInfo.pViewportState = &viewport_state_create_info;
+        pipelineInfo.pRasterizationState = &rasterization_state_create_info;
+        pipelineInfo.pMultisampleState = &multisample_state_create_info;
+        pipelineInfo.pColorBlendState = &color_blend_state_create_info;
+        pipelineInfo.pDepthStencilState = &depth_stencil_create_info;
+        pipelineInfo.layout = _render_pipelines[0].layout;
+        pipelineInfo.renderPass = _framebuffer.render_pass;
+        pipelineInfo.subpass = _main_camera_subpass_tone_mapping;
+        pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+        pipelineInfo.pDynamicState = &dynamic_state_create_info;
+
+        if (vkCreateGraphicsPipelines(m_p_vulkan_context->_device,
+            VK_NULL_HANDLE,
+            1,
+            &pipelineInfo,
+            nullptr,
+            &_render_pipelines[0].pipeline) != VK_SUCCESS)
+        {
+            throw std::runtime_error("create stencil pass 1 graphics pipeline");
+        }
+
+
+        // pass 2: outline pass
+        VkShaderModule vert_shader_module1 =
+            PVulkanUtil::createShaderModule(m_p_vulkan_context->_device, STENCIL_SCALE_VERT);
+        VkShaderModule frag_shader_module1 =
+            PVulkanUtil::createShaderModule(m_p_vulkan_context->_device, STENCIL_SCALE_FRAG);
+        VkPipelineShaderStageCreateInfo vert_pipeline_shader_stage_create_info1{};
+        vert_pipeline_shader_stage_create_info1.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        vert_pipeline_shader_stage_create_info1.stage = VK_SHADER_STAGE_VERTEX_BIT;
+        vert_pipeline_shader_stage_create_info1.module = vert_shader_module;
+        vert_pipeline_shader_stage_create_info1.pName = "main";
+
+        VkPipelineShaderStageCreateInfo frag_pipeline_shader_stage_create_info1{};
+        frag_pipeline_shader_stage_create_info1.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+        frag_pipeline_shader_stage_create_info1.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+        frag_pipeline_shader_stage_create_info1.module = frag_shader_module;
+        frag_pipeline_shader_stage_create_info1.pName = "main";
+
+        VkPipelineShaderStageCreateInfo shader_stages1[] = { vert_pipeline_shader_stage_create_info,
+                                                           frag_pipeline_shader_stage_create_info };
+
+        VkPipelineDepthStencilStateCreateInfo depth_stencil_create_info1{};
+        depth_stencil_create_info1.stencilTestEnable = VK_TRUE;
+        depth_stencil_create_info1.depthTestEnable = VK_FALSE;
+        depth_stencil_create_info1.back.compareOp = VK_COMPARE_OP_NOT_EQUAL;
+        depth_stencil_create_info1.back.failOp = VK_STENCIL_OP_KEEP;
+        depth_stencil_create_info1.back.depthFailOp = VK_STENCIL_OP_KEEP;
+        depth_stencil_create_info1.back.passOp = VK_STENCIL_OP_REPLACE;
+        depth_stencil_create_info1.front = depth_stencil_create_info.back;
+
+        VkGraphicsPipelineCreateInfo pipelineInfo1{};
+        pipelineInfo1.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipelineInfo1.stageCount = 2;
+        pipelineInfo1.pStages = shader_stages;
+        pipelineInfo1.pVertexInputState = &vertex_input_state_create_info;
+        pipelineInfo1.pInputAssemblyState = &input_assembly_create_info;
+        pipelineInfo1.pViewportState = &viewport_state_create_info;
+        pipelineInfo1.pRasterizationState = &rasterization_state_create_info;
+        pipelineInfo1.pMultisampleState = &multisample_state_create_info;
+        pipelineInfo1.pColorBlendState = &color_blend_state_create_info;
+        pipelineInfo1.pDepthStencilState = &depth_stencil_create_info;
+        pipelineInfo1.layout = _render_pipelines[1].layout;
+        pipelineInfo1.renderPass = _framebuffer.render_pass;
+        pipelineInfo1.subpass = _main_camera_subpass_tone_mapping;
+        pipelineInfo1.basePipelineHandle = VK_NULL_HANDLE;
+        pipelineInfo1.pDynamicState = &dynamic_state_create_info;
+
+        if (vkCreateGraphicsPipelines(m_p_vulkan_context->_device,
+            VK_NULL_HANDLE,
+            1,
+            &pipelineInfo,
+            nullptr,
+            &_render_pipelines[1].pipeline) != VK_SUCCESS)
+        {
+            throw std::runtime_error("create stencil pass 1 graphics pipeline");
+        }
+    }
 
     void PStencilOutlinePass::setupDescriptorSet()
     {
@@ -72,10 +272,11 @@ namespace Pilot {
 
     void PStencilOutlinePass::draw()
     {
+        // draw pass 1
         if (m_render_config._enable_debug_untils_label)
         {
             VkDebugUtilsLabelEXT label_info = {
-                VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, NULL, "Stencil Outline", {1.0f, 1.0f, 1.0f, 1.0f} };
+                VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, NULL, "Stencil Outline Pass 1", {1.0f, 1.0f, 1.0f, 1.0f} };
             m_p_vulkan_context->_vkCmdBeginDebugUtilsLabelEXT(m_command_info._current_command_buffer, &label_info);
         }
 
@@ -86,6 +287,35 @@ namespace Pilot {
         m_p_vulkan_context->_vkCmdBindDescriptorSets(m_command_info._current_command_buffer,
             VK_PIPELINE_BIND_POINT_GRAPHICS,
             _render_pipelines[0].layout,
+            0,
+            1,
+            &_descriptor_infos[0].descriptor_set,
+            0,
+            NULL);
+
+        vkCmdDraw(m_command_info._current_command_buffer, 3, 1, 0, 0);
+
+
+        // draw pass 2
+        if (m_render_config._enable_debug_untils_label)
+        {
+            m_p_vulkan_context->_vkCmdEndDebugUtilsLabelEXT(m_command_info._current_command_buffer);
+        }
+
+        if (m_render_config._enable_debug_untils_label)
+        {
+            VkDebugUtilsLabelEXT label_info = {
+                VK_STRUCTURE_TYPE_DEBUG_UTILS_LABEL_EXT, NULL, "Stencil Outline Pass 2", {1.0f, 1.0f, 1.0f, 1.0f} };
+            m_p_vulkan_context->_vkCmdBeginDebugUtilsLabelEXT(m_command_info._current_command_buffer, &label_info);
+        }
+
+        m_p_vulkan_context->_vkCmdBindPipeline(
+            m_command_info._current_command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, _render_pipelines[1].pipeline);
+        m_p_vulkan_context->_vkCmdSetViewport(m_command_info._current_command_buffer, 0, 1, &m_command_info._viewport);
+        m_p_vulkan_context->_vkCmdSetScissor(m_command_info._current_command_buffer, 0, 1, &m_command_info._scissor);
+        m_p_vulkan_context->_vkCmdBindDescriptorSets(m_command_info._current_command_buffer,
+            VK_PIPELINE_BIND_POINT_GRAPHICS,
+            _render_pipelines[1].layout,
             0,
             1,
             &_descriptor_infos[0].descriptor_set,
