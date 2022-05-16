a1c37a9133aeccf9f72cc6277506ef611490358b
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-16 13:29:39 +0200

    stencil pass class: 1st commit.

--- engine/source/runtime/function/render/source/vulkan_manager/passes/stecil_outline.cpp
+++ engine/source/runtime/function/render/source/vulkan_manager/passes/stecil_outline.cpp
@@ -9,10 +9,43 @@
 namespace Pilot {
 
     void PStencilOutlinePass::initialize(VkRenderPass render_pass, VkImageView input_attachment)
-    { }
+    {
+        _framebuffer.render_pass = render_pass;
+        setupDescriptorSetLayout();
+        setupPipelines();
+        setupDescriptorSet();
+        updateAfterFramebufferRecreate(input_attachment);
+    }
 
     void PStencilOutlinePass::setupDescriptorSetLayout()
-    { }
+    {
+        _descriptor_infos.resize(1);
+
+        VkDescriptorSetLayoutBinding post_process_global_layout_bindings[1] = {};
+
+        VkDescriptorSetLayoutBinding& post_process_global_layout_input_attachment_binding =
+            post_process_global_layout_bindings[0];
+        post_process_global_layout_input_attachment_binding.binding = 0;
+        post_process_global_layout_input_attachment_binding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        post_process_global_layout_input_attachment_binding.descriptorCount = 1;
+        post_process_global_layout_input_attachment_binding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+
+        VkDescriptorSetLayoutCreateInfo post_process_global_layout_create_info;
+        post_process_global_layout_create_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+        post_process_global_layout_create_info.pNext = NULL;
+        post_process_global_layout_create_info.flags = 0;
+        post_process_global_layout_create_info.bindingCount =
+            sizeof(post_process_global_layout_bindings) / sizeof(post_process_global_layout_bindings[0]);
+        post_process_global_layout_create_info.pBindings = post_process_global_layout_bindings;
+
+        if (VK_SUCCESS != vkCreateDescriptorSetLayout(m_p_vulkan_context->_device,
+            &post_process_global_layout_create_info,
+            NULL,
+            &_descriptor_infos[0].layout))
+        {
+            throw std::runtime_error("create post process global layout");
+        }
+    }
 
     void PStencilOutlinePass::setupPipelines()
     { }
