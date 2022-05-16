ead23599fbc050724ab2d9c38ea43b7d1039d963
chengzimdl@gmail.com <chengzimdl@gmail.com>
2022-05-16 11:47:01 +0200

    fix stencil_solid.vert bugs.

--- engine/shader/glsl/stencil_solid.vert
+++ engine/shader/glsl/stencil_solid.vert
@@ -4,6 +4,13 @@
 
 #include "constants.h"
 
+layout (binding = 0) uniform UBO 
+{
+	mat4 projection;
+	mat4 model;
+	vec4 lightPos;
+} ubo;
+
 layout(location = 0) in vec3 in_position; 
 layout(location = 1) in vec3 in_normal;
 layout(location = 2) in vec3 in_tangent;
@@ -16,5 +23,8 @@ layout(location = 3) out vec2 out_texcoord;
 
 
 void main(){
-
+	out_normal=in_normal;
+	out_texcoord=in_texcoord;
+	highp vec4 world_position = ubo.projection * ubo.model * vec4(in_position.xyz, 1.0);
+	out_world_position = world_position.xyz;
 }
--- engine/source/runtime/function/render/source/vulkan_manager/passes/stecil_outline.cpp
+++ engine/source/runtime/function/render/source/vulkan_manager/passes/stecil_outline.cpp
@@ -5,3 +5,24 @@
 #include "runtime/function/render/include/render/vulkan_manager/vulkan_util.h"
 
 #include <stencil_solid_frag.h>
+
+namespace Pilot {
+
+    void PStencilOutlinePass::initialize(VkRenderPass render_pass, VkImageView input_attachment)
+    { }
+
+    void PStencilOutlinePass::setupDescriptorSetLayout()
+    { }
+
+    void PStencilOutlinePass::setupPipelines()
+    { }
+
+    void PStencilOutlinePass::setupDescriptorSet()
+    { }
+
+    void PStencilOutlinePass::updateAfterFramebufferRecreate(VkImageView input_attachment)
+    { }
+
+    void PStencilOutlinePass::draw()
+    { }
+}
